bc6a29975f9293a205872faeb5fb0b999d9edba8
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-06-05 09:58:21 +0300

    integrated with open api

---
+++ src/main/java/com/project/socialMedia/config/OpenAPIConfig.java
@@ -0,0 +1,40 @@
+package com.project.socialMedia.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Social Media API documentation",
+                version = "1.0",
+                description = """
+                        <p><b>Test credentials:</b><br>
+                        - ---- / -----<br>
+                        """,
+                contact = @Contact(url = "https://github.com/Blynchik", name = "Vadim Sovetnikov", email = "vadimsovetnikov@mail.ru")
+        ),
+        security = @SecurityRequirement(name = "basicAuth")
+)
+public class OpenAPIConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("REST API")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
